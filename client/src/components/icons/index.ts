58901f1a157827edf362b704f492c7525df397f0
Frankie Agent <frankie@oma-ai.com>
2026-04-14 04:33:03 +0000

    Add progress tracking, icon exports, and favicon

---
+++ client/src/components/icons/index.ts
@@ -0,0 +1,11 @@
+// Icon components barrel export
+export { CrownIcon } from './CrownIcon';
+export { GavelIcon } from './GavelIcon';
+export { ScalesIcon } from './ScalesIcon';
+export { ScrollIcon } from './ScrollIcon';
+export { ShareIcon } from './ShareIcon';
+export { TrophyIcon } from './TrophyIcon';
+export { FlameIcon } from './FlameIcon';
+export { ClownFaceIcon } from './ClownFaceIcon';
+export { JesterHatIcon } from './JesterHatIcon';
+export { CourtSealIcon } from './CourtSealIcon';
